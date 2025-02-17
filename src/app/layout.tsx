9a5c6843d1e7f9ccc68b2ac15efe18a3e93de18d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-17 13:36:45 +0100

    changed font to montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const montSerrat = Montserrat({
+    variable: "--font-montserrat",
+    subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montSerrat.className} antialiased`}
       >
         <Nav />
         {children}
