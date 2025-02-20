2a7182dfaa84d1e7890f567135393e5af6400074
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-20 14:40:19 +0100

    changed contact form error messages

--- src/validations/contactSchema.ts
+++ src/validations/contactSchema.ts
@@ -1,9 +1,9 @@
 import {z} from "zod"
 
 export const contactSchema = z.object({
-    name: z.string().min(1),
-    phone: z.string().min(9),
-    description: z.string().min(1),
+    name: z.string().min(1, "Imię i nazwisko nie może być puste"),
+    phone: z.string().min(9, "Telefon musi mieć minimum 9 znaków"),
+    description: z.string().min(1, "Opis nie może być pusty"),
 })
 
 export type ContactSchema = z.infer<typeof contactSchema>
