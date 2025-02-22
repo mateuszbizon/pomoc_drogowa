e0e6e30bc38f53640604154fe5823f4f5e152311
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-22 11:31:51 +0100

    sending emails

--- src/actions/sendContact.ts
+++ src/actions/sendContact.ts
@@ -1,7 +1,11 @@
 "use server"
 
+import Welcome from "@/emails/welcome";
 import { addContactService } from "@/services/contacts";
 import { contactSchema, ContactSchema } from "@/validations/contactSchema";
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendContact(contact: ContactSchema) {
     const resultValidation = contactSchema.safeParse(contact)
@@ -15,6 +19,13 @@ export async function sendContact(contact: ContactSchema) {
 
     try {
         const data = await addContactService(contact)
+
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: 'mat-biz@wp.pl',
+            subject: 'New client',
+            react: Welcome({ name: contact.name, phone: contact.phone, description: contact.description })
+        })
     } catch (error) {
         return {
             success: false,
