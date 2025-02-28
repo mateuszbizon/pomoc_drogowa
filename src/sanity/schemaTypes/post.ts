1633eb838df24efc804c39ab0e7ccff9bcf88f28
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-28 16:14:18 +0100

    added post type

---
+++ src/sanity/schemaTypes/post.ts
@@ -0,0 +1,25 @@
+import { defineField, defineType } from "sanity";
+
+export const postType = defineType({
+    name: "post",
+    title: "Post",
+    type: "document",
+    fields: [
+        defineField({
+            name: 'tytul',
+            type: 'string',
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'opis',
+            type: 'text',
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: {source: 'title'},
+            validation: (rule) => rule.required(),
+        }),
+    ]
+})
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -3,5 +3,7 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title('Kontent')
+    .items([
+        S.documentTypeListItem("post").title("Posty"),
+    ])
